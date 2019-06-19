68200d2ba63f4b780c4a0e6577696509718a491d
Technote <technote.space@gmail.com>
2019-06-20 02:59:35 +0900

    composer for PHP5.3

--- tests/bin/php/install-composer.sh
+++ tests/bin/php/install-composer.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-composer install -n --working-dir=${TRAVIS_BUILD_DIR}
+if [[ ! -z "$IGNORE_PLATFORM_REQS" ]] ; then
+  composer install -n --working-dir=${TRAVIS_BUILD_DIR} --ignore-platform-reqs
+else
+  composer install -n --working-dir=${TRAVIS_BUILD_DIR}
+fi
+
