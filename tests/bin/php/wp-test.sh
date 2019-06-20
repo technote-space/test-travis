5fa63e2605c64d71dd717ae2816c02782fa53581
Technote <technote.space@gmail.com>
2019-06-20 16:24:53 +0900

    fix: execute coveralls after finished specific test (#3)

--- tests/bin/php/wp-test.sh
+++ tests/bin/php/wp-test.sh
@@ -3,3 +3,7 @@
 set -ex
 bash ${TRAVIS_BUILD_DIR}/tests/bin/php/setup-wp-tests.sh
 composer phpunit --working-dir=${TRAVIS_BUILD_DIR}
+
+if [[ ! -z "$COVERAGE_REPORT" ]] ; then
+  composer coveralls --working-dir=${TRAVIS_BUILD_DIR}
+fi
