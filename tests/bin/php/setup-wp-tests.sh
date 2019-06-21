206a9714c657cc30813f83ce8299963d67b3d204
Technote <technote.space@gmail.com>
2019-06-22 01:57:32 +0900

    feat: trivial changes

--- tests/bin/php/setup-wp-tests.sh
+++ tests/bin/php/setup-wp-tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -ex
+
 if [[ -f ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini ]]; then
 	if [[ -z "$COVERAGE_REPORT" ]] ; then
 		phpenv config-rm xdebug.ini
@@ -8,5 +9,6 @@ if [[ -f ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini ]]; the
 else
 	echo "xdebug.ini does not exist"
 fi
+
 bash ${TRAVIS_BUILD_DIR}/tests/bin/php/install-wp-tests.sh wordpress_test root '' localhost ${WP_VERSION}
 bash ${TRAVIS_BUILD_DIR}/tests/bin/php/install-composer.sh
