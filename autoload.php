29e7d92e3a6f68f7877d3bd0db6d77920fcd0d98
Technote <technote.space@gmail.com>
2019-09-09 02:48:31 +0900

    v0.0.25

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/technote-space/test-travis
  * Description: Test plugin for Travis CI
  * Author: Technote
- * Version: 0.0.24
+ * Version: 0.0.25
  * Author URI: https://technote.space
  * Text Domain: test-travis
  * Domain Path: /languages/
