932106b6b311b0d0276f75d5b9569654023a9fa2
Technote <technote.space@gmail.com>
2020-03-29 02:08:15 +0900

    chore: update wp version

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/technote-space/test-travis
  * Description: Test plugin for Travis CI
  * Author: Technote
- * Version: 0.0.29
+ * Version: next-v0.0.30
  * Author URI: https://technote.space
  * Text Domain: test-travis
  * Domain Path: /languages/
